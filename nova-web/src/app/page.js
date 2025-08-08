742b5e6503d2b853f7d6b66cadc74a8d1de23b51
BP-H <blueprinthustler@gmail.com>
2025-08-08 03:42:21 -0400

    Update page.js

--- nova-web/src/app/page.js
+++ nova-web/src/app/page.js
@@ -1,14 +1,3 @@
-import PostCard from "@/components/PostCard";
-
-export const metadata = { title: "superNova_2177" };
-
 export default function Home() {
-  return (
-    <div style={{ display:"grid", gap:14 }}>
-      <h2>Prototype feed (symbolic only)</h2>
-      <PostCard />
-      <PostCard title="Brian Wallace" subtitle="Aid worker at Garcia, Robinson and Joseph • 2nd" />
-      <PostCard title="Rebecca Bauer" subtitle="Herbalist at Cooper, Clark and Wallace • 1st" />
-    </div>
-  );
+  return <h1 style={{color:"hotpink"}}>NEW HOMEPAGE TEST</h1>
 }
