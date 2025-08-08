0e8c943a094e37e99df81994266f609965fe9429
BP-H <blueprinthustler@gmail.com>
2025-08-08 03:44:54 -0400

    Update page.js

--- nova-web/src/app/page.js
+++ nova-web/src/app/page.js
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1 style={{color:"hotpink"}}>NEW HOMEPAGE TEST</h1>
+import Sidebar from '../components/Sidebar';
+import TopBar from '../components/TopBar';
+import Feed from './feed/page'; // or wherever your feed logic lives
+
+export default function HomePage() {
+  return (
+    <div className="flex bg-black min-h-screen text-white">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <TopBar />
+        <main className="p-4">
+          <Feed />
+        </main>
+      </div>
+    </div>
+  );
 }
