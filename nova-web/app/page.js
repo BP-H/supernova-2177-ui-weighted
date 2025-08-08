0ec3b6da73f08ae24973a4e137e0706f4e14986b
BP-H <blueprinthustler@gmail.com>
2025-08-08 02:44:20 -0400

    Update page.js

--- nova-web/app/page.js
+++ nova-web/app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="p-6">
       <h1 className="text-2xl font-semibold">superNova_2177</h1>
-      <p className="opacity-70 mt-2">Hello from Next.js — build should pass now.</p>
+      <p className="opacity-70 mt-2">Hello from Next.js — build should pass nowww.</p>
     </main>
   );
 }
