ce156a48eecb8e4bcb1a3d413696d1af7dc8fd9d
BP-H <blueprinthustler@gmail.com>
2025-08-08 15:37:59 -0400

    Update layout.js

--- nova-web/src/app/layout.js
+++ nova-web/src/app/layout.js
@@ -1,17 +1,14 @@
-// File: app/layout.js
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'superNova_2177',
   description: 'Mathematically sucked into a void of innovation',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-nova-dark text-white">
-        {children}
-      </body>
+      <body className="bg-nova-dark text-white">{children}</body>
     </html>
-  )
+  );
 }
