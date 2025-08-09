c770eadb1e7c89f1708ff4d550c1aaafd9e14670
BP-H <blueprinthustler@gmail.com>
2025-08-08 23:15:09 -0400

    Update page.js

--- nova-web/app/universe/page.js
+++ nova-web/app/universe/page.js
@@ -1,10 +1,8 @@
-import dynamic from "next/dynamic";
-const Universe3D = dynamic(() => import("@/components/Universe3D"), { ssr: false });
-
 export default function UniversePage() {
   return (
-    <div style={{height: "100vh", background:"#000"}}>
-      <Universe3D />
+    <div className="sn-post">
+      <div style={{ fontWeight: 700 }}>Universe</div>
+      <div style={{ opacity: 0.8 }}>3D scene coming soon.</div>
     </div>
   );
 }
